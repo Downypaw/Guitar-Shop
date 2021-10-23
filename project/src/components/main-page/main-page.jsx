cd252ab3746c4ead289584871229b7fa797ea0a5
Таня <tanamalceva274@gmail.com>
2021-10-23 12:25:37 +0300

    Сделан подвал

--- project/src/components/main-page/main-page.jsx
+++ project/src/components/main-page/main-page.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../header/header';
+import Footer from '../footer/footer';
 import {AppRoute} from '../../const.js';
 
 export default function MainPage() {
@@ -7,7 +8,9 @@ export default function MainPage() {
     <>
       <Header page={AppRoute.INDEX}/>
       <main className="page-main">
+        <h1 className="visually-hidden">Магазин Guitar Shop</h1>
       </main>
+      <Footer page={AppRoute.INDEX}/>
     </>
   );
 }
