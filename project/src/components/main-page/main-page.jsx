aa2846266eaba1a2258e6ad8810ed0e7a2004630
Таня <tanamalceva274@gmail.com>
2021-10-24 15:43:17 +0300

    Добавит верстку шлавной страницы

--- project/src/components/main-page/main-page.jsx
+++ project/src/components/main-page/main-page.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Header from '../header/header';
+import Filter from '../filter/filter';
+import Sorting from '../sorting/sorting';
+import Catalog from '../catalog/catalog';
 import Footer from '../footer/footer';
 import {AppRoute} from '../../const.js';
 
@@ -8,12 +11,23 @@ export default function MainPage() {
     <>
       <Header page={AppRoute.INDEX}/>
       <main className="page-main">
-        <h1 className="visually-hidden">Магазин Guitar Shop</h1>
-        <h2 className="page-title">Каталог гитар</h2>
-        <ul className="page-path">
-          <li className="page-path__item">Главная</li>
-          <li className="page-path__item">Каталог</li>
-        </ul>
+        <div className="container">
+          <h1 className="visually-hidden">Магазин Guitar Shop</h1>
+          <h2 className="page-title">Каталог гитар</h2>
+          <ul className="page-path">
+            <li className="page-path__item">
+              <a className="page-path__link">Главная</a>
+            </li>
+            <li className="page-path__item">
+              <a className="page-path__link">Каталог</a>
+            </li>
+          </ul>
+          <div className="page-main__content">
+            <Filter />
+            <Sorting />
+            <Catalog />
+          </div>
+        </div>
       </main>
       <Footer page={AppRoute.INDEX}/>
     </>
--- project/src/const.js
+++ project/src/const.js
@@ -2,3 +2,9 @@ export const AppRoute = {
   INDEX: '/',
   CART: '/cart',
 };
+
+export const DEFAULT_RATING = 5;
+
+export const ITEMS_COUNT_ON_PAGE = 9;
+
+export const START_PAGE = 1;
